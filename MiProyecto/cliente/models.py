cb0962935416a445bc4bc54e1dc964deec32e4db
Bruno Diaz <brunoodiaz.88@gmail.com>
2023-05-21 19:01:40 -0300

    Prueba de vista y modelo de cliente

--- MiProyecto/cliente/models.py
+++ MiProyecto/cliente/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+class Pais(models.Model):
+
+    nombre = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.nombre
+class Cliente(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email=models.EmailField
+    nacimiento = models.DateField(null=True)
+    pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+
+        return f"{self.nombre} {self.apellido}"
 # Create your models here.
